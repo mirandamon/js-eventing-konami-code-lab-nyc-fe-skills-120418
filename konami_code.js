31b87b868d08bc63a634f41d2634fd31899f3ab7
Nathan Miranda <nate845@gmail.com>
2019-01-31 23:18:27 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -7,10 +7,12 @@ const checkKeyCode = (event) => {
   const whatTheyPressed = event.keyCode
   if (whatTheyPressed === code[tracker]) {
     tracker += 1
-  } else if (tracker === 10) {
-      alert('You won, yay!')
   } else {
     tracker = 0
+  }
+  
+  if (tracker === 10) {
+    alert('You won, yay!')
   }
   console.log(tracker)
 }
